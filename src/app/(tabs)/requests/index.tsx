2f0ffe8143f7b37bb45b4bea8211e9c0895906c5
abnerdouglas <abner.rmachado@gmail.com>
2025-04-25 08:55:03 -0300

    Revert "fix: ajuste na exibição dos status das solicitações"
    This reverts commit 8a882515b3cb5aac0274f75a298547ff91d553d9.

--- src/app/(tabs)/requests/index.tsx
+++ src/app/(tabs)/requests/index.tsx
@@ -13,7 +13,7 @@ import { Input, InputSlot, InputIcon, InputField } from "@/components/ui/input"
 import { useFocusEffect } from "expo-router";
 import StatusFilterPicker from "@/src/components/StatusFilterPicker"
 
-export default function ListRequests() {
+export default function ListRequests(){
     const [requests, setRequests] = useState<Request[]>([])
     const [refreshing, setRefreshing] = useState(false)
     const [searchText, setSearchText] = useState("")
@@ -31,11 +31,11 @@ export default function ListRequests() {
         setRefreshing(false)
     }, [searchText, status])
 
-    useEffect(() => {
+    useEffect(()=>{
         fetchRequests()
     }, [searchText, status])
 
-    useFocusEffect(
+     useFocusEffect(
         useCallback(() => {
             fetchRequests()
         }, [searchText, status])
@@ -46,37 +46,37 @@ export default function ListRequests() {
     }, [searchText, status])
 
     return (
-        <Box className="flex-auto w-screen">
-            <Heading size="2xl" className="px-3 py-3 my-3">Solicitações</Heading>
-            <Input className="w-5/6 mx-auto border border-[#8a2be2]">
-                <InputSlot className="p-2">
-                    <InputIcon as={SearchIcon} />
-                </InputSlot>
-                <InputField placeholder="Procurar por nome ou código..." value={searchText} onChangeText={v => setSearchText(v)} />
-            </Input>
-            <Box className="mx-auto my-2">
-                <StatusFilterPicker selectedValue={status} setSelectedValue={setStatus} />
-                <Text className="mx-4">{requests.length} resultados</Text>
-            </Box>
-            <AddButton href='/requests/new' />
-            <FlatList
-                data={requests}
-                keyExtractor={item => item._id}
-                refreshControl={
-                    <RefreshControl refreshing={refreshing} onRefresh={refreshRequests} />
-                }
-                renderItem={({ item }: ListRenderItemInfo<Request>) =>
-                    <TouchableOpacity
-                        className="flex-row justify-between items-center p-3"
-                        onPress={() => router.push({ pathname: '/requests/[request_id]', params: { request_id: item._id } })}>
-                        <Box className="gap-1 flex-1">
-                            <Text className="text-lg">{item.title}</Text>
-                            <Text className="text-gray-300 text-sm">#{item.code}</Text>
-                        </Box>
-                        <StatusBadge status={item.status} />
-                    </TouchableOpacity>
-                }>
-            </FlatList>
+    <Box className="flex-auto w-screen">
+        <Heading size="2xl" className="px-3 py-3 my-3">Solicitações</Heading>
+        <Input className="w-5/6 mx-auto border border-[#8a2be2]">
+            <InputSlot className="p-2">
+                <InputIcon as={SearchIcon} />
+            </InputSlot>
+            <InputField placeholder="Procurar por nome ou código..." value={searchText} onChangeText={v => setSearchText(v)} />
+        </Input>
+        <Box className="mx-auto my-2">
+            <StatusFilterPicker selectedValue={status} setSelectedValue={setStatus}/>
+            <Text className="mx-4">{requests.length} resultados</Text>
         </Box>
+        <AddButton href='/requests/new'/>
+        <FlatList 
+        data={requests}
+        keyExtractor={item => item._id}
+        refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={refreshRequests}/>
+        }
+        renderItem={({item}: ListRenderItemInfo<Request>) => 
+        <TouchableOpacity
+        className="flex-row justify-between items-center p-3"
+        onPress={() => router.push({pathname: '/requests/[request_id]', params: { request_id: item._id} })}>
+            <Box className="gap-1 flex-1">
+                <Text className="text-lg">{item.title}</Text>
+                <Text className="text-gray-300 text-sm">#{item.code}</Text>
+            </Box>
+            <StatusBadge status={item.status}/>
+        </TouchableOpacity>
+    }>
+        </FlatList>
+    </Box>
     )
 }
--- src/components/StatusFilterPicker.tsx
+++ src/components/StatusFilterPicker.tsx
@@ -1,8 +1,8 @@
 import { Box } from "@/components/ui/box"
-import { Text } from 'react-native'
-import { View } from 'react-native'
+import {Text} from 'react-native'
+import { ScrollView } from "react-native-gesture-handler"
 
-interface PropsType {
+interface PropsType{
     selectedValue: string | undefined
     setSelectedValue: React.Dispatch<React.SetStateAction<string | undefined>>
 }
@@ -14,34 +14,35 @@ const statusList = [
     "Recusado"
 ]
 
-export default function StatusFilterPicker(props: PropsType) {
-    const { selectedValue, setSelectedValue } = props
+export default function StatusFilterPicker(props: PropsType){
+    const {selectedValue, setSelectedValue} = props
 
     return (
-        <Box className="flex flex-row flex-wrap gap-3 p-4">
-            <Text
-                className="px-4 py-1 rounded-full font-medium"
-                onPress={() => setSelectedValue(undefined)}
+        <ScrollView horizontal alwaysBounceHorizontal showsHorizontalScrollIndicator={false}>
+            <Box className="flex flex-row gap-3 p-4">
+                <Text className="px-3 py-1 rounded-full font-medium" onPress={() => setSelectedValue(undefined)} 
                 style={{
-                    backgroundColor: selectedValue === undefined ? '#d6c2f1' : '#d1d5db',
-                    color: selectedValue === undefined ? '#6200ee' : ''
-                }}
-            >
-                Todos
-            </Text>
-            {statusList.map(opt =>
-                <Text
-                    className="px-4 py-1 rounded-full font-medium"
+                        backgroundColor: selectedValue === undefined ? '#d6c2f1' : '#d1d5db',
+                        color: selectedValue === undefined ? '#6200ee': ''
+                    }}
+                >
+                    Todos
+                </Text>
+                {statusList.map(opt => 
+                    <Text 
+                    className="px-3 py-1 rounded-full font-medium"
                     style={{
                         backgroundColor: selectedValue === opt ? '#d6c2f1' : '#d1d5db',
-                        color: selectedValue === opt ? '#6200ee' : ''
+                        color: selectedValue === opt ? '#6200ee': ''
                     }}
                     key={opt}
                     onPress={() => setSelectedValue(opt)}
-                >
-                    {opt}
-                </Text>
-            )}
-        </Box>
+                    >
+                        {opt}
+                    </Text>
+                )}
+            </Box>
+        </ScrollView>
     )
+
 }
