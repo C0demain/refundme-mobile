82f3a9a3f7940af39bfd8823bfd12b7a93497c39
abnerdouglas <abner.rmachado@gmail.com>
2025-05-10 14:05:49 -0300

    feat: adiciona botao de logout a tela de perfil do usuario

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -7,110 +7,110 @@ import React from 'react';
 export default function TabLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-    <Tabs
-      screenOptions={{
-        headerShown: true,
-        tabBarActiveTintColor: '#8a2be2',
-        tabBarInactiveTintColor: 'gray',
-        tabBarHideOnKeyboard: true,
-        tabBarStyle: {
-          height: 80, // 👈 aumenta a altura da tab bar
-          paddingBottom: 20, // 👈 opcional: ajusta o ícone mais pra cima
-          paddingTop: 10,
-        },
-        tabBarLabelStyle: {
-          fontSize: 12,
-        },
-        headerStyle: {
-          height: 100, // 👈 aumenta a altura do header
-        },
-        headerTitle: () => (
-          <Image
-            source={require('../../assets/images/logo.png')} // ajuste o caminho conforme seu projeto
-            style={{ 
-              width: 150, 
-              height: 100, 
-              resizeMode: 'contain',
-              marginTop: 10, 
-            }}
-          />
-        ),
-        headerTitleAlign: 'center', // opcional: centraliza a imagem
-      }}
-    >
-      <Tabs.Screen
-        name="refund/[request_id]"
-        options={{
-          href: null
+      <Tabs
+        screenOptions={{
+          headerShown: true,
+          tabBarActiveTintColor: '#8a2be2',
+          tabBarInactiveTintColor: 'gray',
+          tabBarHideOnKeyboard: true,
+          tabBarStyle: {
+            height: 80, // 👈 aumenta a altura da tab bar
+            paddingBottom: 20, // 👈 opcional: ajusta o ícone mais pra cima
+            paddingTop: 10,
+          },
+          tabBarLabelStyle: {
+            fontSize: 12,
+          },
+          headerStyle: {
+            height: 100, // 👈 aumenta a altura do header
+          },
+          headerTitle: () => (
+            <Image
+              source={require('../../assets/images/logo.png')} // ajuste o caminho conforme seu projeto
+              style={{
+                width: 150,
+                height: 100,
+                resizeMode: 'contain',
+                marginTop: 10,
+              }}
+            />
+          ),
+          headerTitleAlign: 'center', // opcional: centraliza a imagem
         }}
-      />
-      <Tabs.Screen
-        name="expenses"
-        options={{
-          title: "Lista de Reembolsos",
-          tabBarIcon: ({ color, size }: { color: string; size: number }) => <Ionicons name="list-outline" size={size} color={color} />,
-          href: null
-        }}
-      />
-      <Tabs.Screen
-        name="requests/index"
-        options={{
-          title: "Solicitações",
-          tabBarIcon: ({ color, size }: { color: string; size: number }) => <Ionicons name="list-outline" size={size} color={color} />,
-        }}
+      >
+        <Tabs.Screen
+          name="refund/[request_id]"
+          options={{
+            href: null
+          }}
         />
-      <Tabs.Screen
-        name='requests/new'
-        options={{
-          href: null
-        }}
+        <Tabs.Screen
+          name="expenses"
+          options={{
+            title: "Lista de Reembolsos",
+            tabBarIcon: ({ color, size }: { color: string; size: number }) => <Ionicons name="list-outline" size={size} color={color} />,
+            href: null
+          }}
         />
-      <Tabs.Screen
-        name='requests/[request_id]/index'
-        options={{
-          href: null
-        }}
+        <Tabs.Screen
+          name="requests/index"
+          options={{
+            title: "Solicitações",
+            tabBarIcon: ({ color, size }: { color: string; size: number }) => <Ionicons name="list-outline" size={size} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name='requests/new'
+          options={{
+            href: null
+          }}
+        />
+        <Tabs.Screen
+          name='requests/[request_id]/index'
+          options={{
+            href: null
+          }}
         />
 
-      <Tabs.Screen
-        name='requests/[request_id]/edit'
-        options={{
-          href: null
-        }}
-      />
-      <Tabs.Screen
-        name='projects/[project_id]/index'
-        options={{
-          href: null
-        }}
-      />
-      <Tabs.Screen
-        name='projects/new'
-        options={{
-          href: null
-        }}
-      />
-      <Tabs.Screen
-        name='requests/[request_id]/delete'
-        options={{
-          href: null
-        }}
-      />
-      <Tabs.Screen
-        name='projects/index'
-        options={{
-          title: "Projetos",
-          tabBarIcon: ({ color, size }: { color: string; size: number }) => <Ionicons name="list-outline" size={size} color={color} />,
-        }}
-      />
-       <Tabs.Screen
-        name='profile/[profile_id]/index'
-        options={{
-          title: "Perfil",
-          tabBarIcon: ({ color, size }: { color: string; size: number }) => <Ionicons name="person-outline" size={size} color={color} />,
-        }}
-      />
-    </Tabs>
+        <Tabs.Screen
+          name='requests/[request_id]/edit'
+          options={{
+            href: null
+          }}
+        />
+        <Tabs.Screen
+          name='projects/[project_id]/index'
+          options={{
+            href: null
+          }}
+        />
+        <Tabs.Screen
+          name='projects/new'
+          options={{
+            href: null
+          }}
+        />
+        <Tabs.Screen
+          name='requests/[request_id]/delete'
+          options={{
+            href: null
+          }}
+        />
+        <Tabs.Screen
+          name='projects/index'
+          options={{
+            title: "Projetos",
+            tabBarIcon: ({ color, size }: { color: string; size: number }) => <Ionicons name="list-outline" size={size} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name='profile/index'
+          options={{
+            title: "Perfil",
+            tabBarIcon: ({ color, size }: { color: string; size: number }) => <Ionicons name="person-outline" size={size} color={color} />,
+          }}
+        />
+      </Tabs>
     </GestureHandlerRootView>
   );
 }
--- src/app/(tabs)/profile/index.tsx
+++ src/app/(tabs)/profile/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useCallback } from "react";
-import { ScrollView, ActivityIndicator, FlatList, View, Text, TouchableOpacity, RefreshControl } from "react-native";
+import { ScrollView, ActivityIndicator, FlatList, View, Text, RefreshControl } from "react-native";
 import { useFocusEffect, useRouter } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getUserById } from "@/src/api/userService/user";
@@ -7,6 +7,7 @@ import { Box } from "@/components/ui/box";
 import { Divider } from "@/components/ui/divider";
 import { Heading } from "@/components/ui/heading";
 import { Accordion, AccordionItem, AccordionHeader, AccordionTrigger, AccordionIcon, AccordionContent } from "@/components/ui/accordion";
+import LogoutButton from "@/src/components/user/LogoutButton";
 import { Ionicons } from "@expo/vector-icons";
 
 type User = {
@@ -22,8 +23,9 @@ export default function UserProfilePage() {
     const [userId, setUserId] = useState<string | null>(null);
     const [user, setUser] = useState<User | null>(null);
     const [loading, setLoading] = useState(false);
-
+    const router = useRouter();
     const [refreshing, setRefreshing] = useState(false);
+    const [isExpanded, setIsExpanded] = useState(false);
 
     const handleRefresh = async () => {
         setRefreshing(true);
@@ -35,6 +37,7 @@ export default function UserProfilePage() {
         try {
             const storedId = await AsyncStorage.getItem("userId");
             setUserId(storedId);
+            console.log("UserId:", storedId);
         } catch (error) {
             console.error("Erro ao obter userId:", error);
         }
@@ -45,6 +48,7 @@ export default function UserProfilePage() {
 
         try {
             setLoading(true);
+            console.log("Buscando usuário com ID:", userId);
             const data = await getUserById(userId);
             setUser(data);
         } catch (error) {
@@ -65,8 +69,19 @@ export default function UserProfilePage() {
     }, [userId]);
 
     if (loading || !user) {
-        return <ActivityIndicator size="large" />;
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <ActivityIndicator size="large" />
+            </View>
+        );
     }
+
+    const handleLogout = async () => {
+        await AsyncStorage.removeItem('token');
+        router.replace('/login'); // redireciona para a tela de login
+    };
+
+    const role = user.role === "admin" ? "Administrador" : user.role === "user" ? "Usuário" : "Desconhecido";
 
     return (
         <ScrollView
@@ -79,22 +94,39 @@ export default function UserProfilePage() {
                 <Heading size="xl" className="mb-4">
                     Perfil do Usuário
                 </Heading>
-                <Text className="text-lg">Nome: {user.name}</Text>
-                <Text className="text-lg">Email: {user.email}</Text>
-                <Text className="text-lg">Função: {user.role.toUpperCase()}</Text>
-                <Text className="text-lg">Solicitações feitas: {user.requests.length}</Text>
-                <Text className="text-lg">Projetos vinculados: {user.projects.length}</Text>
+
+                <Text className="text-lg mt-2">
+                    <Text className="font-bold">Nome: </Text>{user.name}
+                </Text>
+                <Text className="text-lg mt-2">
+                    <Text className="font-bold">Email: </Text>{user.email}
+                </Text>
+                <Text className="text-lg mt-2">
+                    <Text className="font-bold">Função: </Text>{role}
+                </Text>
+                <Text className="text-lg mt-2">
+                    <Text className="font-bold">Solicitações feitas: </Text>{user.requests?.length ?? 0}
+                </Text>
+                <Text className="text-lg mt-2">
+                    <Text className="font-bold">Projetos vinculados: </Text>{user.projects?.length ?? 0}
+                </Text>
             </Box>
 
-            <Divider className="bg-gray-700 my-2" orientation="horizontal" />
+            <Divider className="bg-gray-500 my-2" orientation="horizontal" />
 
             <Box className="bg-white p-4">
                 <Accordion type="single" collapsable defaultValue={[]}>
                     <AccordionItem value="projects">
                         <AccordionHeader>
-                            <AccordionTrigger>
-                                <View style={{ flexDirection: "row" }}>
+                            <AccordionTrigger onPress={() => setIsExpanded(!isExpanded)}>
+                                <View style={{ flexDirection: "row", alignItems: "center", width: '100%' }}>
                                     <Heading size="md">Ver projetos vinculados</Heading>
+                                    <Ionicons
+                                        name={isExpanded ? "chevron-up" : "chevron-down"}
+                                        size={20}
+                                        color="black"
+                                        style={{ marginLeft: 8 }}
+                                    />
                                     <AccordionIcon />
                                 </View>
                             </AccordionTrigger>
@@ -108,7 +140,7 @@ export default function UserProfilePage() {
                                 renderItem={({ item }) => (
                                     <>
                                         <Box className="gap-1 flex-1">
-                                            <Text className="text-md">Projeto ID:</Text>
+                                            <Text className="text-md mt-2">Projeto ID:</Text>
                                             <Text className="text-gray-600 text-sm">{item}</Text>
                                         </Box>
                                     </>
@@ -118,6 +150,19 @@ export default function UserProfilePage() {
                     </AccordionItem>
                 </Accordion>
             </Box>
+
+            <Divider className="bg-gray-500 my-2" orientation="horizontal" />
+
+            <Box className="bg-white">
+                <Accordion type="single" collapsable defaultValue={[]}>
+                    <AccordionItem value="logout">
+                        <AccordionHeader>
+                            <LogoutButton onLogout={handleLogout} />
+                        </AccordionHeader>
+                    </AccordionItem>
+                </Accordion>
+            </Box>
+
         </ScrollView>
     );
 }
